a22c3c8c47f952ab80e008fa79401d5102900e16
PhuongDoz <38605217+PhuongDoz@users.noreply.github.com>
2018-05-12 10:46:28 +0700

    BookstoreWeb

---
+++ BookstoreWeb/BookstoreWeb/Controllers/BookStoreController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookstoreWeb.Models;
+
+using PagedList;
+using PagedList.Mvc;
+
+namespace BookstoreWeb.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        // GET: BookStore
+        dbQLyBanSachDataContext data = new dbQLyBanSachDataContext();
+        private List<SACH> Laysachmoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        public ActionResult Index(int ? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var sachmoi = Laysachmoi(18);
+            return View(sachmoi.ToPagedList(pageNum,pageSize));
+        }
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+
+
+            return PartialView(chude);
+        }
+        public ActionResult Nhaxuatban()
+        {
+            var nxb = from NHAXUATBAN in data.NHAXUATBANs select NHAXUATBAN;
+
+
+            return PartialView(nxb);
+        }
+        public ActionResult SPTheochude(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes
+                       where s.Masach == id
+                       select s;
+            return View(sach.Single());
+        }
+
+    }
+}
---
+++ BookstoreWeb/BookstoreWeb/Models/Giohang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookstoreWeb.Models
+{
+    public class Giohang
+    {
+        dbQLyBanSachDataContext data = new dbQLyBanSachDataContext();
+
+        public int iMasach { set; get; }
+        public string sTensach { set; get; }
+        public string sHinhminhhoa { set; get; }
+        public Double dDongia { set; get; }
+        public int iSoluong { set; get; }
+        public Double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+        }
+        public Giohang(int Masach)
+        {
+            iMasach = Masach;
+            SACH sach = data.SACHes.Single(n => n.Masach == iMasach);
+            sTensach = sach.Tensach;
+            sHinhminhhoa = sach.Hinhminhhoa;
+            dDongia = double.Parse(sach.Dongia.ToString());
+            iSoluong = 1;
+        }
+
+    }
+}
